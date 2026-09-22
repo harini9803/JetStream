c6c0a18f667adac8511ff28b8c3bc27f3bb57a3a
harini9803 <harini.kalyanaraman@multicorewareinc.com>
2026-09-22 18:43:33 +0530

    Fix web-ssr babel plugin path after the workloads/ move

--- workloads/web-ssr/webpack.config.mjs
+++ workloads/web-ssr/webpack.config.mjs
@@ -80,7 +80,7 @@ function createConfig({ filename, minify }) {
                 }],
                 "@babel/preset-react"
               ],
-              plugins: [path.resolve(__dirname, "../utils/BabelCacheBuster.mjs")],
+              plugins: [path.resolve(__dirname, "../../utils/BabelCacheBuster.mjs")],
             },
           },
         },
@@ -90,7 +90,7 @@ function createConfig({ filename, minify }) {
           use: {
             loader: "babel-loader",
             options: {
-              plugins: [path.resolve(__dirname, "../utils/BabelCacheBuster.mjs")],
+              plugins: [path.resolve(__dirname, "../../utils/BabelCacheBuster.mjs")],
             },
           },
         },
